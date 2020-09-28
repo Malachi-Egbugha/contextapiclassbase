7d92c407236d8d3edc9877edf6751adec2ea9d74
Malachi-Egbugha <malachi.egbugha3@gmail.com>
2020-09-28 18:32:49 +0100

    Updates

---
+++ precache-manifest.e1cec221e2397f724536040d34570061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2088347ebd227c4e7ab34dda0eb152f",
+    "url": "/contextapiclassbase/index.html"
+  },
+  {
+    "revision": "4b740242a6deea71b0ed",
+    "url": "/contextapiclassbase/static/css/main.b5b29386.chunk.css"
+  },
+  {
+    "revision": "2410834dacd6655d6982",
+    "url": "/contextapiclassbase/static/js/2.d701aa1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contextapiclassbase/static/js/2.d701aa1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b740242a6deea71b0ed",
+    "url": "/contextapiclassbase/static/js/main.36f68cc6.chunk.js"
+  },
+  {
+    "revision": "dbb99f839d5ced9076c6",
+    "url": "/contextapiclassbase/static/js/runtime-main.14783bf4.js"
+  }
+]);
